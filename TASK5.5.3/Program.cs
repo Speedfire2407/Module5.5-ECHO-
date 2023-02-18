f215ae3a4e6698672039b434db8ba359dfbf861d
Speedfire2407 <Faunsatyr@yandex.ru>
2023-02-18 14:32:29 +0500

    create new folde

--- TASK5.5.3/Program.cs
+++ TASK5.5.3/Program.cs
@@ -11,11 +11,17 @@ namespace TASK5._5._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите что нибудь:");
-            var something = Console.ReadLine();
-            Console.WriteLine("Укажите количество повторений");
-            var depth = int.Parse(Console.ReadLine()); 
-            Echo(something, depth);
+            //Console.WriteLine("Напишите что нибудь:");
+            //var something = Console.ReadLine();
+            //Console.WriteLine("Укажите количество повторений");
+            //var depth = int.Parse(Console.ReadLine()); 
+            //Echo(something, depth);
+            //Console.ReadLine();
+            Console.WriteLine("Введите число");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            var Pow = byte.Parse(Console.ReadLine());
+            Console.WriteLine(PowerUp(number, Pow));
             Console.ReadLine();
         }
 
@@ -30,6 +36,32 @@ namespace TASK5._5._3
                     Console.BackgroundColor = (ConsoleColor)depth;                     
                     Echo(modsomething, depth - 1);
                 }
+            }
+        }
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+        static int PowerUp (int number, byte Pow)
+        {
+            if (Pow == 0)
+            {
+                return 1;
+            }
+            else if (Pow == 1) 
+            {
+                return number;
+            }
+            else
+            {
+                return number * PowerUp(number, --Pow);
             }
         }
     }
