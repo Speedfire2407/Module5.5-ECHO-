bae01b3068f51596647dc49f95856f365285e217
Speedfire2407 <Faunsatyr@yandex.ru>
2023-02-18 13:02:07 +0500

    Task complite

--- TASK5.5.3/Program.cs
+++ TASK5.5.3/Program.cs
@@ -10,6 +10,25 @@ namespace TASK5._5._3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Напишите что нибудь:");
+            var something = Console.ReadLine();
+            Console.WriteLine("Укажите количество повторений");
+            var depth = int.Parse(Console.ReadLine()); 
+            Echo(something, depth);
+            Console.ReadLine();
+        }
+
+        static void Echo (string something, int depth)
+        {
+            Console.WriteLine(something);
+            if (depth > 0)
+            {
+                if (something.Length > 2)
+                {
+                    var modsomething = something.Remove(0, 2);
+                    Echo(modsomething, depth - 1);
+                }
+            }
         }
     }
 }
